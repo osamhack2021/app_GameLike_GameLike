c2bac51bab5b581b6ab0ba7e492c1a71d434271c
Kalready <bagzaru@naver.com>
2021-09-16 11:46:04 +0000

    expo applied

--- APP/App.tsx
+++ APP/App.tsx
@@ -1,7 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import {Text} from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  const textElement = React.createElement(Text, null, 'Hello world');
-  return textElement;
+  return (
+    <SafeAreaView><Text>Hello World! i love it isn't it???wowwowowowwowowowowowowㄹㅇo</Text></SafeAreaView>
+    /*<View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>*/
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
