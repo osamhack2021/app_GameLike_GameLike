32fade5f58d41530270646a82c45cdf992dc8f75
Kalready <bagzaru@naver.com>
2021-09-27 11:44:55 +0000

    App.tsx QuestComponent on

--- APP/App.tsx
+++ APP/App.tsx
@@ -4,18 +4,19 @@ import QuestComponent from './src/Quests/QuestTab';
 import FieldSQLTest from 'src/localdb/FieldSQLTest';
 
 export default function App() {
-  /*
   return (
-   
     <SafeAreaView style={Styles.container}>
       <QuestComponent />
     </SafeAreaView>
-  );*/
-  /*return (
+  );
+
+  /*
+  return (
     <SafeAreaView style={Styles.container}>
       <FieldSQLTest />
     </SafeAreaView>
-  );*/
+  );
+  */
 }
 const Styles = StyleSheet.create({
   container: {
---
+++ APP/src/localdb/Test2.tsx
@@ -0,0 +1,144 @@
+import React, {useCallback, useEffect, useState} from 'react';
+import {
+  Button,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TextInput,
+  useColorScheme,
+  View,
+} from 'react-native';
+import {ToDoItemComponent} from './components/ToDoItem';
+import {ToDoItem} from './models';
+import {
+  getDBConnection,
+  getTodoItems,
+  saveTodoItems,
+  createTable,
+  clearTable,
+  deleteTodoItem,
+} from './services/db-service';
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+  const [todos, setTodos] = useState<ToDoItem[]>([]);
+  const [newTodo, setNewTodo] = useState('');
+  const loadDataCallback = useCallback(async () => {
+    try {
+      const initTodos = [
+        {id: 0, value: 'go to shop'},
+        {id: 1, value: 'eat at least a one healthy foods'},
+        {id: 2, value: 'Do some exercises'},
+      ];
+      const db = await getDBConnection();
+      await createTable(db);
+      const storedTodoItems = await getTodoItems(db);
+      if (storedTodoItems.length) {
+        setTodos(storedTodoItems);
+      } else {
+        await saveTodoItems(db, initTodos);
+        setTodos(initTodos);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }, []);
+  useEffect(() => {
+    loadDataCallback();
+  }, [loadDataCallback]);
+  const addTodo = async () => {
+    if (!newTodo.trim()) return;
+    try {
+      const newTodos = [
+        ...todos,
+        {
+          id: todos.length
+            ? todos.reduce((acc, cur) => {
+                if (cur.id > acc.id) return cur;
+                return acc;
+              }).id + 1
+            : 0,
+          value: newTodo,
+        },
+      ];
+      setTodos(newTodos);
+      const db = await getDBConnection();
+      await saveTodoItems(db, newTodos);
+      setNewTodo('');
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const deleteItem = async (id: number) => {
+    try {
+      const db = await getDBConnection();
+      await deleteTodoItem(db, id);
+      todos.splice(id, 1);
+      setTodos(todos.slice(0));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  return (
+    <SafeAreaView>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <ScrollView contentInsetAdjustmentBehavior="automatic">
+        <View style={[styles.appTitleView]}>
+          <Text style={styles.appTitleText}> ToDo Application </Text>
+        </View>
+        <View>
+          {todos.map(todo => (
+            <ToDoItemComponent
+              key={todo.id}
+              todo={todo}
+              deleteItem={deleteItem}
+            />
+          ))}
+        </View>
+        <View style={styles.textInputContainer}>
+          <TextInput
+            style={styles.textInput}
+            value={newTodo}
+            onChangeText={text => setNewTodo(text)}
+          />
+          <Button
+            onPress={addTodo}
+            title="Add ToDo"
+            color="#841584"
+            accessibilityLabel="add todo item"
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+const styles = StyleSheet.create({
+  appTitleView: {
+    marginTop: 20,
+    justifyContent: 'center',
+    flexDirection: 'row',
+  },
+  appTitleText: {
+    fontSize: 24,
+    fontWeight: '800',
+  },
+  textInputContainer: {
+    marginTop: 30,
+    marginLeft: 20,
+    marginRight: 20,
+    borderRadius: 10,
+    borderColor: 'black',
+    borderWidth: 1,
+    justifyContent: 'flex-end',
+  },
+  textInput: {
+    borderWidth: 1,
+    borderRadius: 5,
+    height: 30,
+    margin: 10,
+    backgroundColor: 'pink',
+  },
+});
+export default App;
