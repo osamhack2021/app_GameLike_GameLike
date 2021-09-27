b7729229593628865aee8d06f22d64f0d19fc32a
ChoHyeonSu <65438056+ChoHyeonSu@users.noreply.github.com>
2021-09-27 10:42:05 +0000

    build test

--- APP/App.tsx
+++ APP/App.tsx
@@ -1,53 +1,24 @@
-import React from 'react';
-import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { NavigationContainer } from '@react-navigation/native';
-import HomeScreen from './HomeScreen';
-import SettingScreen from './SettingScreen';
+import * as React from 'react';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native'; 
+import { createStackNavigator } from '@react-navigation/stack';
 
+import StartScreen from './src/StartScreen';
+import LoginScreen from './src/LoginScreen';
+import MainScreen from './src/MainScreen';
 
+const Stack = createStackNavigator();
 
-const Tab = createBottomTabNavigator();
-/*
-<NavigationContainer>
-        <Tab.Navigator>
-          <Tab.Screen name="Home" component={HomeScreen} />
-          <Tab.Screen name="Settings" component={SettingScreen} />
-        </Tab.Navigator>
-      </NavigationContainer>
-*/
 export default function App() {
   return (
-    
-    <View>
-      {HomeScreen}
-    </View>
-      
-      
-    
+    <NavigationContainer> 
+      <Stack.Navigator initialRouteName="MAIN"> 
+        <Stack.Screen name="START" component={StartScreen} options={{ title: '시작화면' }}/> 
+        <Stack.Screen name="LOGIN" component={LoginScreen} options={{ title: '로그인화면' }}/> 
+        <Stack.Screen name="MAIN" component={MainScreen} options={{ title: '메인화면' }}/>
+      </Stack.Navigator> 
+    </NavigationContainer>
+
+
   );
 }
-const Styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  view1: {
-    flex: 0.25,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#e93e43',
-  },
-  view2: {
-    flex: 0.25,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#f5a941',
-  },
-
-  view3: {
-    flex: 0.5,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#4ebd7a',
-  },
-});
