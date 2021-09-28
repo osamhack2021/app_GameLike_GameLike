884380e955488ae701eabaec7073e487c1cb251a
Kalready <bagzaru@naver.com>
2021-09-28 10:46:07 +0000

    sql load test

--- APP/src/localdb/FieldSQLTest.tsx
+++ APP/src/localdb/FieldSQLTest.tsx
@@ -1,10 +1,73 @@
-import React from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import {Text, ScrollView, StyleSheet, View} from 'react-native';
-import {FieldData} from 'src/Quests/datas/FieldData';
+import {FieldData} from '../Quests/datas/FieldData';
+import * as FT from './FieldTable';
 
-const fields: Array<FieldData> = [];
+const initDatas: Array<FieldData> = [
+  {
+    id: 0,
+    name: 'test',
+    peopleWith: 3277,
+    iconName: 'nonono',
+    dataCreatorId: 'tester',
+    isPublic: false,
+  },
+  {
+    id: 1,
+    name: '리액트',
+    peopleWith: 123,
+    iconName: 'development',
+    dataCreatorId: 'tester',
+    isPublic: true,
+  },
+  {
+    id: 2,
+    name: '등 운동',
+    peopleWith: 5709,
+    iconName: 'sport',
+    dataCreatorId: 'tester',
+    isPublic: true,
+  },
+  {
+    id: 3,
+    name: '수능 미적분',
+    peopleWith: 7777,
+    iconName: 'study',
+    dataCreatorId: 'testerstu',
+    isPublic: true,
+  },
+];
 
 export default function FieldSQLTest() {
+  const [fields, setFields] = useState<FieldData[]>([
+    {
+      id: -1,
+      name: 'initValue',
+      peopleWith: 1111,
+      iconName: 'bbb',
+      dataCreatorId: 'startValue',
+      isPublic: false,
+    },
+  ]);
+  const loadDataCallback = useCallback(async () => {
+    try {
+      const db = await FT.getDBConnection();
+      await FT.createTable(db);
+      const storedItems = await FT.getFieldDatas(db);
+      if (storedItems.length) {
+        setFields(storedItems);
+      } else {
+        await FT.saveFieldDatas(db, initDatas);
+        setFields(initDatas);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }, []);
+  useEffect(() => {
+    loadDataCallback();
+  }, [loadDataCallback]);
+
   const children = fields.map(item => (
     <View>
       <Text>id: {item.id}</Text>
