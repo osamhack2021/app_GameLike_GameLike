f844b9cf4dfb1c9fecd3b9cc91af3685de697a94
wannaBpark <leehun456@hanmail.net>
2021-10-01 13:43:32 +0000

    quest model

---
+++ SERVER/models/quest.js
@@ -0,0 +1,49 @@
+const Sequelize = require('sequelize');
+
+module.exports = class Quest extends Sequelize.Model {
+  static init(sequelize) {
+    return super.init({
+      name: {
+        type: Sequelize.STRING(15),
+        allowNull: false,
+        unique: true,
+      },
+      fieldId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      estimatedTime: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      createdDate: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      creatorId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      isRepeat: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+      },
+      isPublic: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+      },
+    }, {
+      sequelize,
+      timestamps: true,
+      underscored: false,
+      modelName: 'Quest',
+      tableName: 'quests',
+      paranoid: true,
+      charset: 'utf8',
+      collate: 'utf8_general_ci',
+    });
+  }
+
+  static associate(db) {
+  }
+};
--- SERVER/routes/auth.js
+++ SERVER/routes/auth.js
@@ -26,6 +26,26 @@ router.post('/join', isNotLoggedIn, async (req, res, next) => {
   }
 });
 
+router.post('/quest', isLoggedIn, async (req, res, next) => {
+    const { email, nick, password } = req.body;
+    try {
+      const exUser = await User.findOne({ where: { email } });
+      if (exUser) {
+        return res.redirect('/join?error=exist');
+      }
+      const hash = await bcrypt.hash(password, 12);
+      await User.create({
+        email,
+        nick,
+        password: hash,
+      });
+      return res.redirect('/');
+    } catch (error) {
+      console.error(error);
+      return next(error);
+    }
+  });
+
 router.post('/login', isNotLoggedIn, (req, res, next) => {
   passport.authenticate('local', (authError, user, info) => {
     if (authError) {
