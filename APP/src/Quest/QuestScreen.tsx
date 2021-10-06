e5a9739bae879e6fb57563dbd9315dc2d8c30783
Kalready <bagzaru@naver.com>
2021-10-06 13:15:48 +0000

    Questscreen redux applied, stabled

--- APP/src/Quest/QuestScreen.tsx
+++ APP/src/Quest/QuestScreen.tsx
@@ -8,44 +8,30 @@ import {AppState, makeStore} from '../Store';
 import QuestScreenSelector from './QuestScreenSelector';
 
 export default function QuestScreen() {
-  let str = 'a';
-  try {
-    const prevTask = useSelector<AppState, boolean>(
-      state => state.questScreenState.prevTaskChecked,
-    );
-  } catch (error) {
-    if (error instanceof Error) {
-      str = error.message;
-    } else {
-      str = '에러도안뜨냐 무슨';
-    }
-  }
-  // const curTask = useSelector<AppState, boolean>(
-  //   state => state.questScreenState.todayTaskChecked,
-  // );
+  const prevTask = useSelector<AppState, boolean>(
+    state => state.questScreenState.prevTaskChecked,
+  );
+  const curTask = useSelector<AppState, boolean>(
+    state => state.questScreenState.todayTaskChecked,
+  );
 
   const [prevTaskConfirmed, setPrevTaskConfirmed] = useState(false);
   const [todayQuestProduced, setTodayQuestProduced] = useState(false);
-  // return (
-  //   <View>
-  //     <QuestScreenSelector />
-  //     <ScrollView>
-  //       {prevTask ? (
-  //         curTask ? (
-  //           <CurrentQuestScreen />
-  //         ) : (
-  //           <QuestDeciderScreen />
-  //         )
-  //       ) : (
-  //         <PrevQuestScreen />
-  //       )}
-  //       <QuestDeciderScreen />
-  //     </ScrollView>
-  //   </View>
-  // );
   return (
     <View>
-      <Text>{str}</Text>
+      <QuestScreenSelector />
+      <ScrollView>
+        {prevTask ? (
+          curTask ? (
+            <CurrentQuestScreen />
+          ) : (
+            <QuestDeciderScreen />
+          )
+        ) : (
+          <PrevQuestScreen />
+        )}
+        <QuestDeciderScreen />
+      </ScrollView>
     </View>
   );
 }
