fe3a62f1f62752c72026b705c1c54860e8542e04
Kalready <bagzaru@naver.com>
2021-10-09 10:47:16 +0000

    axios debugging 1

--- APP/src/Quest/Connection/ConnectScreen.tsx
+++ APP/src/Quest/Connection/ConnectScreen.tsx
@@ -5,31 +5,39 @@ import axios from 'axios';
 export default function ConnectScreen({navigation}: {navigation: any}) {
   const [log, setLog] = useState('');
   const axGet = useCallback(() => {
-    axios
-      .get('http://52.231.66.60/auth/join')
-      .then(response => {
-        try {
-          setLog(response.data);
-        } catch (e) {
-          setLog('일단 잘 됨');
-        }
-      })
-      .catch(error => {
-        if (error instanceof Error) {
-          setLog(error.message);
-        } else {
+    try {
+      axios
+        .get('http://52.231.66.60/auth/join')
+        .then(response => {
           try {
-            setLog(error);
+            setLog(response.data);
           } catch (e) {
-            setLog('');
-            if (e instanceof Error) {
-              setLog(e.message);
-            } else {
-              setLog('error ocurred');
+            setLog('일단 잘 됨');
+          }
+        })
+        .catch(error => {
+          if (error instanceof Error) {
+            setLog(error.message);
+          } else {
+            try {
+              setLog(error);
+            } catch (e) {
+              setLog('');
+              if (e instanceof Error) {
+                setLog(e.message);
+              } else {
+                setLog('error ocurred');
+              }
             }
           }
-        }
-      });
+        });
+    } catch (e) {
+      if (e instanceof Error) {
+        setLog(e.message);
+      } else {
+        setLog('뭔가 잘못됨');
+      }
+    }
   }, []);
   const axPost = useCallback(() => {
     axios
