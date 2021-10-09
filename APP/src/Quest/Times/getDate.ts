b3f6d9a892e3f53f4be8b7b98633935261486d87
Kalready <bagzaru@naver.com>
2021-10-09 03:52:12 +0000

    getdate for of modified

--- APP/src/Quest/Times/getDate.ts
+++ APP/src/Quest/Times/getDate.ts
@@ -9,16 +9,16 @@ export default function getDate(date: string = '') {
       const day = arr[0].split('-');
       const time = arr[1].split(':');
       const d: number[] = [];
-      for (let i in day) {
+      for (let i of day) {
         d.push(parseInt(i, 10));
       }
-      for (let i in time) {
+      for (let i of time) {
         d.push(parseInt(i, 10));
       }
-      Alert.alert('input:' + date);
-      Alert.alert('day:' + day.toString());
-      Alert.alert('time:' + time.toString());
-      Alert.alert(d.toString());
+      // Alert.alert('input:' + date);
+      // Alert.alert('day:' + day.toString());
+      // Alert.alert('time:' + time.toString());
+      // Alert.alert(d.toString());
       return new Date(d[0], d[1], d[2], d[3], d[4], d[5]);
     } catch (e) {
       if (e instanceof Error) {
