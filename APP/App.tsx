d20a3eb2d2963123f571fdb66cf0b8d14f578b82
ChoHyeonSu <65438056+ChoHyeonSu@users.noreply.github.com>
2021-10-09 10:44:40 +0000

    axios test

--- APP/App.tsx
+++ APP/App.tsx
@@ -1,24 +1,42 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native'; 
-import { createStackNavigator } from '@react-navigation/stack';
+import {Text, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 
 import StartScreen from './src/StartScreen';
 import LoginScreen from './src/LoginScreen';
 import MainScreen from './src/MainScreen';
+import RegisterScreen from './src/RegisterScreen';
+
+// SplashScreen을 추가할지 고민중 (user_id가 저장되어 있는 경우 바로 MainScreen으로 이동하도록) => StartScreen으로 일단 대체
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer> 
-      <Stack.Navigator initialRouteName="MAIN"> 
-        <Stack.Screen name="START" component={StartScreen} options={{ title: '시작화면' }}/> 
-        <Stack.Screen name="LOGIN" component={LoginScreen} options={{ title: '로그인화면' }}/> 
-        <Stack.Screen name="MAIN" component={MainScreen} options={{ title: '메인화면' }}/>
-      </Stack.Navigator> 
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="START">
+        <Stack.Screen
+          name="START"
+          component={StartScreen}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="LOGIN"
+          component={LoginScreen}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="MAIN"
+          component={MainScreen}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="REGISTER"
+          component={RegisterScreen}
+          options={{headerShown: false}}
+        />
+      </Stack.Navigator>
     </NavigationContainer>
-
-
   );
 }
