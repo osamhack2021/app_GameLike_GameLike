841932935c78ac2c84261fadd2dce29da4294014
wannaBpark <leehun456@hanmail.net>
2021-10-11 11:21:59 +0000

    deleted login condition

--- SERVER/routes/auth.js
+++ SERVER/routes/auth.js
@@ -38,7 +38,7 @@ router.get('/join', async(req, res, next) => {
 	  }
 });
 
-router.post('/login', isNotLoggedIn, (req, res, next) => {
+router.post('/login', /*isNotLoggedIn,*/ (req, res, next) => {
   passport.authenticate('local', (authError, user, info) => {
     if (authError) {
       console.error(authError);
