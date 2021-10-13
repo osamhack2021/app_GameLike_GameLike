0ef4c5391376dd63552475db494835a31dd56bc1
ChoHyeonSu <65438056+ChoHyeonSu@users.noreply.github.com>
2021-10-13 11:15:57 +0000

    logout test

--- APP/src/Component/ProfileScreen.tsx
+++ APP/src/Component/ProfileScreen.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Button, Alert} from 'react-native';
+import {useState, useEffect, useCallback, createRef} from 'react';
+import axios from 'axios';
 
 // default값들임, 데이터 불러왔으면 지우기
 var name = 'name';
@@ -7,6 +9,27 @@ var email = 'email';
 var day = 'day';
 
 export default function ProfileScreen() {
+  const [log, setLog] = useState('');
+
+  const onClickLogout = () => {
+    axios
+      .get('http://52.231.66.60/auth/logout')
+      .then(response => {
+        try {
+          setLog(`로그아웃 성공이오 ${JSON.stringify(response.data)}`);
+        } catch (e) {
+          setLog('로그아웃 성공 했지만');
+        }
+      })
+      .catch(error => {
+        if (error instanceof Error) {
+          setLog(error.message);
+        } else {
+          setLog('로그아웃 error ocurred');
+        }
+      });
+  };
+
   return (
     <View>
       <View>
@@ -17,6 +40,8 @@ export default function ProfileScreen() {
         <Text>이메일 : {email} </Text>
         <Text>입대일자 : {day} </Text>
         <Text>D-DAY : </Text>
+        <Button title="로그아웃" onPress={() => onClickLogout()} />
+        <Text>log</Text>
       </View>
     </View>
   );
