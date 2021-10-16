279b70885f022a4b5f9109a8cdca83cd267c795d
Kalready <bagzaru@naver.com>
2021-10-16 11:25:19 +0900

    reload functions axios applied

--- APP/src/Quest/Datas/Connection/reloadTodayExpected.ts
+++ APP/src/Quest/Datas/Connection/reloadTodayExpected.ts
@@ -2,11 +2,46 @@
 //     post:   date
 //     res:    date 값이 일치하는 같은 모든 expected data
 
+import axios from 'axios';
+import {Alert} from 'react-native';
+import getDateString from 'src/Quest/Times/getDateString';
 import {ExpectedData} from '..';
 import testDatas from './expectedTestDatas';
 
 export function reloadTodayExpected(): ExpectedData.DataType[] {
-  return testDatas;
+  let log = '';
+  const date = getDateString();
+  try {
+    axios
+      .post('http://52.231.66.60/auth/join', {date: date})
+      .then(response => {
+        try {
+          log = 'get well:' + JSON.stringify(response.data);
+          Alert.alert(JSON.stringify(response.data));
+          //데이터 반환하기
+          //arr = response.data
+        } catch (e) {
+          log = '일단 잘 됨';
+        }
+
+        return testDatas;
+      })
+      .catch(error => {
+        if (error instanceof Error) {
+          log = error.message;
+        } else {
+          log = 'error ocurred while getting expected datas';
+        }
+      });
+  } catch (e) {
+    if (e instanceof Error) {
+      log = 'axios:' + e.message;
+    } else {
+      log = 'axios 실행 중 오류';
+    }
+    Alert.alert(log);
+  }
+  return [];
 }
 
 export default reloadTodayExpected;
--- APP/src/connection/reloadExpected.tsx
+++ APP/src/connection/reloadExpected.tsx
@@ -12,35 +12,6 @@ import getDateString from 'src/Quest/Times/getDateString';
 function reloadExpected(): [string, ExpectedData.DataType[]] {
   let arr: ExpectedData.DataType[] = [];
   return ['test', arr];
-  // let log = '';
-  // const date = getDateString();
-  // try {
-  //   axios
-  //     .post('http://52.231.66.60/auth/join', {date: date})
-  //     .then(response => {
-  //       try {
-  //         log = 'get well:' + JSON.stringify(response.data);
-  //         //데이터 반환하기
-  //         //arr = response.data
-  //       } catch (e) {
-  //         log = '일단 잘 됨';
-  //       }
-  //     })
-  //     .catch(error => {
-  //       if (error instanceof Error) {
-  //         log = error.message;
-  //       } else {
-  //         log = 'error ocurred while getting expected datas';
-  //       }
-  //     });
-  // } catch (e) {
-  //   if (e instanceof Error) {
-  //     log = 'axios:' + e.message;
-  //   } else {
-  //     log = 'axios 실행 중 오류';
-  //   }
-  // }
-  // return [log, arr];
 }
 
 export default reloadExpected;
