0caae7fc68a86b63e61a130d827a5b41f8abefa0
wannaBpark <leehun456@hanmail.net>
2021-10-16 07:15:15 +0000

    fixed : performed endTime2

--- SERVER/routes/quest.js
+++ SERVER/routes/quest.js
@@ -120,11 +120,11 @@ router.post('/expectedToday', async (req, res, next) => {
 
 // 5번 Performed endTime 수정
 router.post('/updatePe', async(req, res, next) =>{ // 프로필 닉네임 수정예제
-  const {userId, endTime} = req.body;
+  const {userId, startTime} = req.body;
   try {
     // const performed = await Performed.update({where})
     await Quest.update({ endTime: req.body.endTime }, {
-      where: { userId: req.body.userId, startTime:endTime },
+      where: { userId: req.body.userId, startTime:startTime, endTime:NULL },
     });
     res.json({
       message: "Performed Update SUCCESS!", success : true
